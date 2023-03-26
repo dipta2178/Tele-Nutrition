5c2878a0be3d3fc260a1de1f2717aebfa8a364f0
Dipta Voumick <45919158+dipta2178@users.noreply.github.com>
2023-03-26 23:10:16 +0600

    Add files via upload

---
+++ admin_logout.php
@@ -0,0 +1,20 @@
+<!DOCTYPE>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>admin_logout</title>
+</head>
+
+<body>
+<?php
+error_reporting(0);
+
+include("connection.php");
+session_start();
+session_destroy();
+unset($SESSION['admin_id']);
+
+	  header("Location:index.php");
+	  ?>
+</body>
+</html>
