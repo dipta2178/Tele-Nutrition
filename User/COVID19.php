9323a5309246205361d8d82bf64d8a0344a27f7f
Dipta Voumick <45919158+dipta2178@users.noreply.github.com>
2023-04-09 22:39:32 +0600

    Create COVID19.php

---
+++ User/COVID19.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+		<style>
+			body{
+				background-color: #e6e6ff;
+			}
+			#example1 {
+			  
+			  background-color: #8585ad;
+			  background-repeat: no-repeat;
+			  background-size: 100%;
+
+			}
+
+			#example2 {
+				background: url("cov1.gif");
+			
+			  background-repeat: no-repeat;
+			  background-size: 100%;
+			}
+		img {
+		  display: block;
+		  margin-left: auto;
+		  margin-right: auto;
+		}
+		</style>
+<body>
+
+<!-- Navigation -->
+<nav class="w3-bar w3-black">
+  <a href="#home" class="w3-button w3-bar-item">Home</a>
+  <a href="login.php" class="w3-button w3-bar-item">Login</a>
+  <a href="registration.php" class="w3-button w3-bar-item">Signup</a>
+</nav>
+<div id="example1">
+<h2><span style="color: black"><marquee behavior="scroll" direction="left"
+           onmouseover="this.stop();"
+           onmouseout="this.start();"><b>For COVID-19 emergency Call 333,16263,999, 9512264, 9514988, and 01712080983</b></marquee></span></h2>
+
+<H3 class="w3-wide" style="text-align:center;">Coronavirus disease (COVID-19) advices for the public as per WHO</H3>
+
+<!-- Slide Show -->
+<section>
+  <img class="mySlides" src="covid-1.jpg"
+  style="width:60%">
+  <img class="mySlides" src="covid-2.jpg"
+  style="width:60%">
+  <img class="mySlides" src="covid-3.jpg"
+  style="width:60%">
+  <img class="mySlides" src="covid-4.jpg"
+  style="width:60%">
+  <img class="mySlides" src="covid-5.jpg"
+  style="width:60%">
+  <img class="mySlides" src="covid-6.jpg"
+  style="width:60%">
+  <img class="mySlides" src="covid-7.jpg"
+  style="width:60%">
+  <img class="mySlides" src="covid-8.jpg"
+  style="width:60%">
+</section><br></div><br>
+<div id="example2">
+<!-- Band Description -->
+<section class="w3-container w3-center w3-content" style="max-width:900px">
+  <h2 class="w3-wide"><b>Coronavirus</b></h2>
+  <p class="w3-opacity"><b><i>A pandemic</i></b></p>
+  <p class="w3-justify"><b>The coronavirus disease 2019 (COVID-19) pandemic has now spread through the entire world and emerged as a major public health threat. COVID-19 is a highly transmittable and pathogenic viral infection caused by severe acute respiratory syndrome coronavirus 2. This novel pathogenic virus emerged in Wuhan, China, and has rapidly spread around the world,1 with 210 countries, including Bangladesh, reporting more than 2.4 million confirmed cases of COVID-19, leading to 169,006 deaths as of April 22, 2020.2 Many developed countries in the world have imposed uniquely severe isolation measures in an attempt to attenuate the mortality and disease transmission of the virus, and the World Health Organization (WHO) declared the situation a public health emergency of international concern. The first COVID-19 case in Bangladesh was identified on March 8, 2020. Since then, 120 deaths of 3772 confirmed cases have been reported to date. The total number of cases is rising rapidly, with a high mortality rate (3.18%).3 As the number of COVID-19 infections continues to rise in Bangladesh (Fig. 1 ), an emergency public health response is urgently required to mitigate the severity of the COVID-19 outbreak, which has the potential to result in devastating social, economic, and political crisis.</b></p><br>
+</section></div>
+
+<!-- Band Members -->
+<section class="w3-row-padding w3-center w3-light-grey">
+	<h2><b>Here’s a list of numbers that could come in handy in case of emergency:</b></h2>
+  <article class="w3-third">
+  <h3>DHAKA</h3>
+   
+    <p>IEDCR: 02-9898796, 10655, 01944333222, email: iedcrcovid19@gmail.com,
+
+	Institute of Public Health: 02-8821361;
+
+	Armed Forces Institute of Pathology: 01769-016616;
+
+	Bangabandhu Sheikh Mujib Medical University: 01866-637482;
+
+	Dhaka Shishu (Children) Hospital and Child Health Research Foundation: 02-48110117
+
+	9 am - 2 pm: 01880156639, 01701265193, 01755507465
+
+	2 pm - 7 pm: 01701265184, 01709958448, 01880156635;
+
+	International Centre for Diarrhoeal Disease Research, Bangladesh (icddr,b): 09666-771100;
+
+	Institute of Developing Science and Health Initiative and Health Services (ideSHi) - 01793-163304;
+
+	Dhaka National Institute of Laboratory Medicine: 02-913917;
+
+	Dhaka Medical College Hospital: 02-55165088</p>
+  </article>
+  <article class="w3-third">
+    <h3>OUTSIDE DHAKA</h3>
+   
+    <p>Bangladesh Institute of Tropical and Infectious Diseases, Chattogram: 02-88075042, 02-88075043;
+
+	Rangpur Medical College: 0521-63388;
+
+	Rajshahi Medical College: 0721-772150;
+
+	Mymensingh Medical College: 01306497095, 01306497096;
+
+	Sylhet MAG Osmani Medical College Hospital: 01700943274;
+
+	Khulna Medical College: 081-760350;
+
+	Barishal Sher-e-Bangla Medical College: 0431-2173547, 01711352119;
+
+	Cox's Bazar Medical College: 01713160093, 01675233455, 01715282637, 01711860111</p>
+  </article>
+  <article class="w3-third">
+   <h3>OTHER HOTLINES</h3>
+    
+    <p>Directorate General of Health Services (DGHS): 16263;
+
+	National Call Centre: 333;
+
+	To report rumours or disinformation, dial: 999 or 9512264, 9514988;
+
+	For burial assistance, contact joint secretaries to the health ministry on: 01712080983 and 01552204208;
+
+	National helpline for violence against women and children: 109;
+
+	To provide information on suspected COVID-19 patients, visit: https://corona.gov.bd/affected
+
+	</p>
+  </article>
+</section>
+
+<!-- Footer -->
+<footer class="w3-container w3-padding-64 w3-center w3-black w3-xlarge">
+  <a href="https://www.facebook.com"><i class="fa fa-facebook-official"></i></a>
+  <a href="https://www.pinterest.com/"><i class="fa fa-pinterest-p"></i></a>
+  <a href="https://twitter.com/"><i class="fa fa-twitter"></i></a>
+  <a href="https://www.flickr.com/"><i class="fa fa-flickr"></i></a>
+  <a href="https://www.linkedin.com/"><i class="fa fa-linkedin"></i></a>
+  
+</footer>
+
+<script>
+// Automatic Slideshow - change image every 3 seconds
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}
+  x[myIndex-1].style.display = "block";
+  setTimeout(carousel, 3000);
+}
+</script>
+
+</body>
+</html>
+
