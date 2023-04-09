8a5d2b1aa227aabada304b8ab0c15f7040ebc168
Dipta Voumick <45919158+dipta2178@users.noreply.github.com>
2023-04-09 22:40:30 +0600

    Add files via upload

---
+++ User/pregnantWomen.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+		<style>
+			body{
+				background-color: #e6e6ff;
+			}
+			#example1 {
+			  
+			  background-color: #CFEBD4;
+			  background-repeat: no-repeat;
+			  background-size: 100%;
+
+			}
+
+			#example2 {
+				background-color: #CFEBD4;
+			
+			  background-repeat: no-repeat;
+			  background-size: 100%;
+			}
+		img {
+		  display: block;
+		  margin-left: auto;
+		  margin-right: auto;
+		}
+		</style>
+<body>
+<!-- Navigation -->
+<nav class="w3-bar w3-black">
+  <a href="#home" class="w3-button w3-bar-item">Home</a>
+  <a href="login.php" class="w3-button w3-bar-item">Login</a>
+  <a href="registration.php" class="w3-button w3-bar-item">Signup</a>
+</nav> 
+
+<div id="example1">
+
+<H3 class="w3-wide" style="text-align:center;">Ideal Foods to Eat During Pregnancy</H3>
+
+<!-- Slide Show -->
+<section>
+  <img class="mySlides" src="IdealfoodForPregnantWomen.jpg"
+  style="width:70%" "height: 80%">
+</section><br></div>
+
+<div id="example2">
+<!-- Band Description -->
+<section class="w3-container  w3-content" style="max-width:900px">
+
+  <h2 class="w3-wide"><b>The following foods are beneficial to your health and fetal development during pregnancy:</b></h2>
+
+  <ul class="w3-ul" style="width:90%">
+  <li><span class="w3-badge">1</span> Vegetables: carrots, sweet potatoes, pumpkin, spinach, cooked greens, tomatoes and red sweet peppers (for vitamin A and potassium)</li>
+  <li><span class="w3-badge">2</span> Fruits: cantaloupe, honeydew, mangoes, prunes, bananas, apricots, oranges, and red or pink grapefruit (for potassium)</li>
+  <li><span class="w3-badge">3</span> Dairy: fat-free or low-fat yogurt, skim or 1% milk, soymilk (for calcium, potassium, vitamins A and D)</li>
+  <li><span class="w3-badge">4</span> Grains: ready-to-eat cereals/cooked cereals (for iron and folic acid)</li>
+  <li><span class="w3-badge">5</span> Proteins: beans and peas; nuts and seeds; lean beef, lamb and pork; salmon, trout, herring, sardines and pollock</li>
+</ul>
+  <br>
+
+  <h2 class="w3-wide"><b>Fluid Intake During Pregnancy:</b></h2>
+  <p class="w3-justify"><b>Fluid intake is also an important part of pregnancy nutrition. Follow these recommendations for fluid intake during pregnancy:</b></p><br>
+  <ul class="w3-ul">
+  <li><span class="w3-badge">1</span> You can take in enough fluids by drinking several glasses of water each day, in addition to the fluids in juices and soups. Talk to your health care provider or midwife about restricting your intake of caffeine and artificial sweeteners.</li>
+  <li><span class="w3-badge">2</span> Avoid all forms of alcohol.</li>
+</ul><br>
+</section></div>
+<div id="example2">
+<section class="w3-container  w3-content" style="max-width:900px">
+
+  <h2 class="w3-wide"><b>Australian Dietary Guidelines serving recommendations for pregnant women, by food group:</b></h2>
+
+  <table class="w3-table-all">
+    <tr>
+      <th>Food group</th>
+      <th>Serves per day </th>
+      <th>Example serving size </th>
+    </tr>
+    <tr>
+      <td>Vegetables and legumes/beans </td>
+      <td>18 years or under: 5 
+19–50 years: 5</td>
+      <td>½ cup cooked vegetables 
+½ cup cooked or canned* beans, peas or lentils
+1 cup green leafy or raw salad vegetables
+½ cup sweet corn
+½ medium potato or other starchy vegetables 
+1 medium tomato</td>
+    </tr>
+    <tr>
+      <td>Fruit</td>
+      <td>18 years or under: 2 
+19–50 years: 2</td>
+      <td>1 medium fruit, such as apple, banana, orange
+2 small fruits, such as apricots, kiwi fruits or plums
+1 cup diced or canned fruit (no added sugar) 
+
+Or only occasionally
+
+125 ml (½ cup) fruit juice (no added sugar)
+30 g dried fruit (such as 4 apricot halves, 1 ½ tablespoons sultanas)</td>
+    </tr>
+    <tr>
+      <td>Grain (cereal) foods, mostly wholegrain and/or high-fibre varieties</td>
+      <td>18 years or under: 8 
+19–50 years: 8½</td>
+      <td>1 slice bread, ½ medium roll or flat bread (40 g)
+½ cup cooked rice, pasta, noodles, barley, buckwheat, semolina, polenta, burghul or quinoa
+½ cup cooked porridge, 2/3 cup wheat cereal flakes, ¼ cup muesli
+3 crispbreads
+1 crumpet, small English muffin or scone</td>
+    </tr>
+    <tr>
+      <td>Lean meats and poultry, fish, eggs, tofu, nuts and seeds and legumes/beans</td>
+      <td>18 years or under: 3½ 
+19–50 years: 3 ½</td>
+      <td>65 g cooked lean meats, such as beef, lamb, veal, pork, goat or kangaroo 
+(90–100 g raw)
+80 g cooked lean poultry, such as chicken, turkey (100 g raw)
+100 g cooked fish fillet (115 g raw) or one small can of fish
+2 large eggs
+1 cup cooked or canned* legumes/beans, such as lentils, chickpeas or split peas
+170 g tofu
+30 g nuts or seeds, nut/seed paste*</td>
+    </tr>
+    <tr>
+      <td>Milk, yoghurt, cheese and/or alternatives, mostly reduced fat</td>
+      <td>18 years or under: 3 ½ 
+19–50 years: 2 ½ </td>
+      <td>1 cup (250 ml) fresh, UHT long-life, reconstituted powdered milk or buttermilk
+½ cup (120 ml) evaporated milk
+2 slices (40 g) hard cheese, such as cheddar
+¾ cup (200 g) yoghurt
+1 cup (250 ml) soy, rice or other cereal drink, with at least 100 mg of added calcium per 100 ml</td>
+    </tr>
+  </table>
+
+</section><br></div>
+<section class="w3-row-padding w3-center w3-#e6e6ff">
+	<h2><b>Here’s a list of numbers that could come in handy in case of emergency:</b></h2>
+  <article class="w3-third">
+  <h3>DHAKA</h3>
+   
+    <p>01770......</p>
+  </article>
+  <article class="w3-third">
+    <h3>OUTSIDE DHAKA</h3>
+   
+    <p>017.......</p>
+  </article>
+  <article class="w3-third">
+   <h3>OTHER HOTLINES</h3>
+    
+    <p>019......</p>
+  </article>
+</section>
+
+<!-- Footer -->
+<footer class="w3-container w3-padding-64 w3-center w3-black w3-xlarge">
+  <a href="https://www.facebook.com"><i class="fa fa-facebook-official"></i></a>
+  <a href="https://www.pinterest.com/"><i class="fa fa-pinterest-p"></i></a>
+  <a href="https://twitter.com/"><i class="fa fa-twitter"></i></a>
+  <a href="https://www.flickr.com/"><i class="fa fa-flickr"></i></a>
+  <a href="https://www.linkedin.com/"><i class="fa fa-linkedin"></i></a>
+  
+</footer>
+</body>
+</html>
